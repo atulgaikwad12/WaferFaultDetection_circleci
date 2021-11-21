696a5107e959eef1cf9ced5320f3a828bfe8a5b9
atulgaikwad12 <atulanilgaikwad12@gmail.com>
2021-11-21 18:23:07 +0530

    initial wafer_circleci commit

---
+++ main.py
@@ -0,0 +1,32 @@
+
+from flask import Flask
+
+from wsgiref import simple_server
+
+from flask import Flask, session, request, Response, jsonify
+
+
+
+import atexit
+import uuid
+import os
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    return "Flask app is running and I am changing something."
+
+port = int(os.getenv("PORT", 5001))
+
+if __name__ == "__main__":
+    host = '0.0.0.0'
+    #app.run()
+    httpd = simple_server.make_server(host=host, port=port, app=app)
+    #httpd = simple_server.make_server(host='127.0.0.1', port=5000, app=app)
+    # print("Serving on %s %d" % (host, port))
+    httpd.serve_forever()
+
+# commands to setup proj
+#conda create -n env_name python==3.7
+#pip install -r requirements.txt
